22052404091b3098cbd309a52ab7789a2213bd05
jancpkgaming23 <72095721+jancpkgaming23@users.noreply.github.com>
2020-10-08 23:26:32 +0700

    bot.py

--- PythonViewBot/bot.py
+++ PythonViewBot/bot.py
@@ -1,14 +1,14 @@
 import time;
 from selenium import webdriver;
 
-#time to refresh page (seconds)
-Timer = 120
+#time to refresh page (minute)
+Timer = 5
 
 #youtube link
-link = 'https://www.youtube.com/watch?v=hW_WFUs3hfQ'
+link = 'https://youtu.be/yiIsp1Yt8rY'
 
 #number of views
-views = 20
+views = 250.000
 
 driver = webdriver.Chrome()
 driver.get(link)
